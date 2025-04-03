ae6bf42b7fb5be1f0ca48b84a929316119c5bcf7
Qinghong Min <qinghongmin@Qinghongs-MacBook-Pro.local>
2025-04-03 21:00:00 +1000

    login auth create

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
+from django.contrib.auth import authenticate
 import json
 
 @csrf_exempt
