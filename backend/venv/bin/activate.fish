62d59b0edc2a137e887b9eda6923664a1596dc49
Qinghong Min <qinghongmin@Qinghongs-MacBook-Pro.local>
2025-04-03 16:22:43 +1000

    fix problems in setting.py

--- backend/venv/bin/activate.fish
+++ backend/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/qinghongmin/Downloads/ecocahce/venv"
+set -gx VIRTUAL_ENV "/Users/qinghongmin/Downloads/ecocahce/backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
