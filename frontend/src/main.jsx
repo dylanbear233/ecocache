de95ca5074c9cbe2ea489632e413558a77aa0b34
Qinghong Min <qinghongmin@Qinghongs-MacBook-Pro.local>
2025-04-08 19:47:45 +1000

    route modified

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import Dashboard from "./pages/Dashboard"; 
+import Dashboard from "./pages/Dashboard";
 import RequireAuth from "./RequireAuth";
 import ShareDiscovery from "./pages/ShareDiscovery";
 
@@ -19,16 +19,12 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/" element={<App />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route
-          path="/dashboard"
-          element={
+        <Route path="/dashboard" element={
             <RequireAuth>
               <Dashboard />
-            </RequireAuth>
-          }
-        />      </Routes>
+            </RequireAuth>}/>
         <Route path="/share" element={<ShareDiscovery />} />
-
+      </Routes>
     </HashRouter>
   </React.StrictMode>
 );
